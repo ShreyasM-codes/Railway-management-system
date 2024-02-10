04be50b9e576b5c4a35ba1d86f595dd31af1bb10
Shreyas M <137000011+ShreyasM-codes@users.noreply.github.com>
2024-02-10 09:52:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,540 @@
+<!DOCTYPE html>
+
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Online Ticket Reservation</title>
+	
+	
+<!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body >
+
+<!-- [ LOADERs ]
+
+================================================================================================================================-->
+	
+    <div class="preloader">
+    
+<div class="loader theme_background_color">
+        
+<span></span>
+      
+    
+</div>
+</div>
+<!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html">Online<span class="themecolor"> T</span>ickets</a>
+        
+</div>
+
+        
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" class="page-scroll"><h3>Home</h3></a></li>
+            
+
+                                   
+<li><a href="#one" class="page-scroll"><h3>About us</h3></a></li>
+                                 
+
+          
+
+<li><a href="admin/" class="page-scroll"><h3>Login</h3></a></li>
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+
+   
+<!-- [/NAV]
+ 
+============================================================================================================================--> 
+    
+   
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Railway"></span></h1>
+ 
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Management System"></span></h1>
+<h1 class="themecolor" > <b>RAILWAY RESERVATION SYSTEM </B></h1>
+       
+<p class="main-heading-text">WELCOME TO,<br/>ONLINE TICKET RESERVATION SYSTEM</p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-custom theme_background_color">Reserve Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+ 
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title">We are Creative</h1>
+        
+<p class="main-heading-text">South Indian Railways-Mysore</p>
+        
+<div class="btn-bar">
+          
+<a href="#" class="btn btn-custom theme_background_color">Get Started</a>
+          
+<a href="#" class="btn btn-custom-outline">Contact Us</a>
+        
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+    
+ 
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [ABOUT US]
+ 
+============================================================================================================================-->
+ 
+<section class="aboutus white-background black" id="one">
+     
+<div class="container">
+         
+<div class="row">
+             
+<div class="col-md-12 text-center black">
+                 
+<h3 class="title">ABOUT <span class="themecolor">US</span></h3>
+            
+<p class="a-slog"><B>Shreyas M</B>   and  <B>Shriya D</B></p> 
+             
+</div>
+         
+</div>
+         
+<div class="gap">
+             
+         
+</div>
+         
+         
+<div class="row about-box">
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-newspaper-o"></i>
+              
+<h4>Class and section</h4>
+              
+<p class="black">5th semester "A" sec</p>
+            
+</div> <!-- / margin -->
+          
+</div> <!-- /col -->
+          
+<div class="col-sm-4 about-line text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-diamond"></i>
+              
+<h4>USN</h4>
+              
+<p class="black">4MH20CS090 & 4MH20CS091</p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-area-chart"></i>
+              
+<h4>MINI PROJECT</h4>
+              
+<p class="black">Data Base Management System</p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+        
+</div> <!-- /row -->
+         
+         
+         
+         
+     
+</div>
+ </section>
+ 
+ 
+ 
+<!-- [/ABOUTUS]
+ 
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+ 
+ 
+<!-- [INSPIRATION]
+ 
+============================================================================================================================-->
+ 
+<section class="inspiration" id="four">
+     
+<div class="overlay">
+         
+<div class="container">
+             
+<div class="row">
+                
+ <article class="col-md-12 text-center">
+           
+ <div class="intermediate-container">
+             
+ <div class="subheading">
+                 
+ <h4>Are You Ready To <span class="themecolor">Enjoy?</span></h4>
+           
+   </div>
+             
+ <div class="heading">
+              
+  <h2>South Indian Railways</h2>
+          
+    </div>
+              
+<div class="">
+              
+  <a class="btn btn-custom-outline" href="#"><span>get started</span></a>
+        
+      </div>
+        
+    </div>
+       
+   </article>
+       
+      </div>
+    
+    </div>
+    
+ </div>
+ 
+</section>
+ 
+ 
+<!-- [/INSPIRATION]
+ 
+
+============================================================================================================================-->
+ 
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+ 
+ 
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [CONTACT]
+ 
+============================================================================================================================-->
+ 
+<!--sub-form-->
+<section class="sub-form text-center" id="eight">
+  
+<div class="container">
+    <div class="col-md-12">
+        
+<h3 class="title">MINI PROJECT - <span class="themecolor"> 01</span></h3>
+            
+<p class="lead">DATA BASE MANAGEMENT SYSTEM</p>
+    
+</div> 
+    
+<div class="row">
+        
+<div class="col-md-3 col-sm-3"></div>
+      
+<div class="col-md-6 center-block col-sm-6 ">
+        
+<form id="mc-form">
+          
+<div class="input-group">
+            
+<input type="email" class="form-control" placeholder="Email Address" required id="mc-email">
+           
+<span class="input-group-btn">
+            
+<button type="submit" class="btn btn-default">send <i class="fa fa-envelope"></i> </button>
+            
+</span> </div>
+          
+<label for="mc-email" id="mc-notification"></label>
+       
+ </form>
+      
+</div>
+   
+ </div>
+  
+</div>
+
+</section>
+
+<!--sub-form end--> 
+
+
+ 
+ 
+<!-- [/CONTACT]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [FOOTER]
+ 
+============================================================================================================================-->
+ 
+
+<footer class="site-footer section-spacing text-center " id="eight">
+    
+  
+<div class="container">
+    
+<div class="row">
+      
+<div class="col-md-4">
+        
+<p class="footer-links"><a href="#"><b>VISIT</a> <a href="#"><b>AGAIN</a></p>
+ 
+</div>
+      
+     
+<div class="col-md-4"> 
+        
+<!--social-->
+        
+        
+<ul class="social">
+          
+<li>4MH20CS090</li>
+          
+<li>4MH20CS091</li>
+        
+        
+</ul>
+        
+        
+<!--social end--> 
+        
+      
+</div>
+    
+</div>
+  
+</div>
+
+</footer>
+
+ 
+ 
+ 
+<!-- [/FOOTER]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+</div>
+ 
+
+<!-- [ /WRAPPER ]
+
+=============================================================================================================================-->
+
+	
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+
+	
+<script src="js/plugins.js"></script>
+        
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+
+</html>
