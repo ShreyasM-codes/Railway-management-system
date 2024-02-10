018b3df45577cb090ee6d86f79a220ea7abab5b0
Shreyas M <137000011+ShreyasM-codes@users.noreply.github.com>
2024-02-10 09:50:39 +0530

    Add files via upload

---
+++ database/south_train.sql
@@ -0,0 +1,175 @@
+CREATE TABLE `accomodation` (
+  `acc_id` int(11) NOT NULL,
+  `acc_type` varchar(35) NOT NULL,
+  `acc_price` double NOT NULL,
+  `acc_slot` int(4) NOT NULL
+) ;
+
+INSERT INTO `accomodation` (`acc_id`, `acc_type`, `acc_price`, `acc_slot`) VALUES
+(1, 'Sitting', 350, 30),
+(2, 'Economy A', 390, 30),
+(3, 'Economy B', 390, 30),
+(4, 'Tourist', 490, 30),
+(5, 'Cabin', 600, 30),
+(6, 'Deluxe', 700, 30);
+
+CREATE TABLE `booked` (
+  `book_id` int(11) NOT NULL,
+  `book_by` varchar(50) NOT NULL,
+  `book_contact` varchar(15) NOT NULL,
+  `book_address` varchar(100) NOT NULL,
+  `book_name` varchar(100) NOT NULL,
+  `book_age` int(11) NOT NULL,
+  `book_gender` varchar(15) NOT NULL,
+  `book_departure` date NOT NULL,
+  `dest_id` int(11) NOT NULL,
+  `acc_id` int(11) NOT NULL,
+  `origin_id` int(11) NOT NULL,
+  `book_tracker` varchar(35) NOT NULL
+) ;
+
+CREATE TABLE `destination` (
+  `dest_id` int(11) NOT NULL,
+  `dest_destination` varchar(35) NOT NULL
+) ;
+
+INSERT INTO `destination` (`dest_id`, `dest_destination`) VALUES
+(1,'Mysore'),
+(3,'Mandya'),
+(4,'Chennai'),
+(5,'manglore'),
+(6,'raichur');
+
+CREATE TABLE `origin` (
+  `origin_id` int(11) NOT NULL,
+  `origin_desc` varchar(35) NOT NULL
+) ;
+
+INSERT INTO `origin` (`origin_id`, `origin_desc`) VALUES
+(1,'Banglore'),
+(2,'Mysore'),
+(3,'Mandya'),
+(4,'Chennai'),
+(5,'manglore');
+
+CREATE TABLE `status` (
+  `stat_id` int(11) NOT NULL,
+  `stat_desc` varchar(20) NOT NULL
+) ;
+
+INSERT INTO `status` (`stat_id`, `stat_desc`) VALUES
+(1, 'Paid'),
+(2, 'Refunded');
+
+CREATE TABLE `transaction` (
+  `trans_id` int(11) NOT NULL,
+  `trans_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `trans_payment` double NOT NULL,
+  `trans_passenger` varchar(100) NOT NULL,
+  `trans_age` int(11) NOT NULL,
+  `trans_gender` varchar(15) NOT NULL,
+  `acc_id` int(11) NOT NULL,
+  `origin_id` int(11) NOT NULL,
+  `dest_id` int(11) NOT NULL,
+  `stat_id` int(11) DEFAULT '1',
+  `trans_refunded` tinyint(4) NOT NULL DEFAULT '0'
+) ;
+
+INSERT INTO `transaction` (`trans_id`, `trans_time`, `trans_payment`, `trans_passenger`, `trans_age`, `trans_gender`, `acc_id`, `origin_id`, `dest_id`, `stat_id`, `trans_refunded`) VALUES
+(1, '2022-02-27 16:06:37', 351, 'Yashas', 23, 'Male', 2, 1, 3, 1, 1),
+(2, '2023-01-02 15:08:34', 280.8, 'Sagar', 25, 'Male', 2, 2, 4, 1, 1);
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_account` varchar(50) NOT NULL,
+  `user_password` varchar(35) NOT NULL
+) ;
+
+
+
+ALTER TABLE `accomodation`
+  ADD PRIMARY KEY (`acc_id`);
+
+ALTER TABLE `booked`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `dest_id` (`dest_id`,`acc_id`),
+  ADD KEY `acc_id` (`acc_id`),
+  ADD KEY `origin_id` (`origin_id`);
+
+--
+-- Indexes for table `destination`
+--
+ALTER TABLE `destination`
+  ADD PRIMARY KEY (`dest_id`);
+
+--
+-- Indexes for table `origin`
+--
+ALTER TABLE `origin`
+  ADD PRIMARY KEY (`origin_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`stat_id`);
+
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`trans_id`),
+  ADD KEY `acc_id` (`acc_id`,`origin_id`,`dest_id`,`stat_id`),
+  ADD KEY `origin_id` (`origin_id`),
+  ADD KEY `dest_id` (`dest_id`),
+  ADD KEY `stat_id` (`stat_id`);
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+-- AUTO_INCREMENT for table `accomodation`
+--
+ALTER TABLE `accomodation`
+  MODIFY `acc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `booked`
+--
+ALTER TABLE `booked`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `destination`
+--
+ALTER TABLE `destination`
+  MODIFY `dest_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `origin`
+--
+ALTER TABLE `origin`
+  MODIFY `origin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `stat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+
+ALTER TABLE `booked`
+  ADD CONSTRAINT `booked_ibfk_1` FOREIGN KEY (`dest_id`) REFERENCES `destination` (`dest_id`),
+  ADD CONSTRAINT `booked_ibfk_2` FOREIGN KEY (`acc_id`) REFERENCES `accomodation` (`acc_id`),
+  ADD CONSTRAINT `booked_ibfk_3` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`origin_id`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`acc_id`) REFERENCES `accomodation` (`acc_id`),
+  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`origin_id`),
+  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`dest_id`) REFERENCES `destination` (`dest_id`),
+  ADD CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`stat_id`) REFERENCES `status` (`stat_id`);
